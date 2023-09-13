aa516a4fa4afa33cc2823c7e2e3b43a77dcd5f76
a20140132 <131292588+a20140132@users.noreply.github.com>
2023-09-13 12:13:19 -0500

    Add files via upload

---
+++ integrador.js
@@ -0,0 +1,11 @@
+alert("Bienvenidos a la pizzeria Papa johns's")
+entrada=parseInt(prompt("Ingrese el valor de la entrada"))
+console.log("El valor de la entrada es:",entrada)
+segundo=parseInt(prompt("Ingrese el valor del segundo "))
+console.log("El valor del segundo es:",segundo)
+postre=parseInt(prompt("Ingrese el valor del postre"))
+console.log("El valor del postre es:",postre)
+total=entrada+segundo+postre
+igv=total*18/100
+console.log("El valor del total es:",total)
+console.log("El valor del total con igv es:",igv)
